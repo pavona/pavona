de91d09df6cee2d54c55529ad4aec949c2cca4af
Ryan Torok <rtorok@zerorisc.com>
2026-03-09 12:16:24 -0400

    [dj] Add Darjeeling Verilator tests to CI
    Signed-off-by: Ryan Torok <rtorok@zerorisc.com>

--- ci/scripts/run-verilator-tests.sh
+++ ci/scripts/run-verilator-tests.sh
@@ -5,30 +5,30 @@
 
 set -e
 
+set -x
+set -e
+
+. util/build_consts.sh
+
+if [ $# == 0 ]; then
+    echo >&2 "Usage: run-verilator-tests.sh <top> <target_pattern_file> [bazel options...]"
+    echo >&2 "E.g. ./run-verilator-tests.sh earlgrey list_of_tests.txt --cache_test_results=no"
+    exit 1
+fi
+top="$1"
+target_pattern_file="$2"
+shift 2
+
 # Increase the test_timeout due to slow performance on CI
 
 ./bazelisk.sh test \
+    --//hw/top=${top} \
     --build_tests_only=true \
     --test_timeout=2400,2400,4000,-1 \
     --local_test_jobs=8 \
     --local_resources=cpu=8 \
-    --test_tag_filters=verilator,-broken \
+    --test_tag_filters=verilator,-broken,-skip_in_ci \
     --test_output=errors \
     --//hw:verilator_options=--threads,1 \
     --//hw:make_options=-j,8 \
-    //sw/device/tests:aes_smoketest_sim_verilator \
-    //sw/device/tests:uart_smoketest_sim_verilator \
-    //sw/device/tests:crt_test_sim_verilator \
-    //sw/device/tests:acc_randomness_test_sim_verilator \
-    //sw/device/tests:acc_irq_test_sim_verilator \
-    //sw/device/tests:kmac_mode_cshake_test_sim_verilator \
-    //sw/device/tests:kmac_mode_kmac_test_sim_verilator \
-    //sw/device/tests:flash_ctrl_test_sim_verilator \
-    //sw/device/tests:usbdev_test_sim_verilator \
-    //sw/device/silicon_creator/lib/drivers:hmac_functest_sim_verilator \
-    //sw/device/silicon_creator/lib/drivers:uart_functest_sim_verilator \
-    //sw/device/silicon_creator/lib/drivers:retention_sram_functest_sim_verilator \
-    //sw/device/silicon_creator/lib/drivers:alert_functest_sim_verilator \
-    //sw/device/silicon_creator/lib/drivers:watchdog_functest_sim_verilator \
-    //sw/device/silicon_creator/lib:irq_asm_functest_sim_verilator \
-    //sw/device/silicon_creator/rom:rom_epmp_test_sim_verilator
+    --target_pattern_file="${target_pattern_file}" "$@"
