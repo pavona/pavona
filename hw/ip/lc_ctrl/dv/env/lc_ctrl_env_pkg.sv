lowRISC/opentitan

[hw,rtl,top,lc_ctrl,otp_ctrl] Allow reading partitions in RMA state
Signed-off-by: David Schrammel <davidschrammel@rivosinc.com>

hw/ip/lc_ctrl/dv/env/lc_ctrl_env_pkg.sv
@@ -62,6 +62,7 @@ package lc_ctrl_env_pkg;
     lc_ctrl_pkg::lc_tx_t lc_iso_part_sw_wr_en_o;
     lc_ctrl_pkg::lc_tx_t lc_keymgr_en_o;
     lc_ctrl_pkg::lc_tx_t lc_escalate_en_o;
+    lc_ctrl_pkg::lc_tx_t lc_rma_sw_rd_override_en_o;
   } lc_outputs_t;
 
   // error injection
@@ -145,56 +146,56 @@ package lc_ctrl_env_pkg;
   // verilog_format: off
   const lc_outputs_t EXP_LC_OUTPUTS[NUM_STATES] = {
     // Raw (fixed size array index 0)
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // TestUnlock0
-    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off},
+    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off, Off},
     // TestLock0
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // TestUnlock1
-    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off},
+    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off, Off},
     // TestLock1
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // TestUnlock2
-    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off},
+    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off, Off},
     // TestLock2
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // TestUnlock3
-    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off},
+    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off, Off},
     // TestLock3
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // TestUnlock4
-    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off},
+    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off, Off},
     // TestLock4
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // TestUnlock5
-    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off},
+    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off, Off},
     // TestLock5
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // TestUnlock6
-    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off},
+    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off, Off},
     // TestLock6
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // TestUnlock7
-    {On,  Off, On,  On,  Off, Off, Off, Off, On,  Off, Off},
+    {On,  Off, On,  On,  Off, Off, Off, Off, On,  Off, Off, Off},
     // Dev: lc_creator_seed_sw_rw_en_o (On if device is not personalized),
     // lc_seed_hw_rd_en_o (On if device is personalized)
-    {Off, Off, On,  On,  On,  On,  On,  Off, On,  On,  Off},
+    {Off, Off, On,  On,  On,  On,  On,  Off, On,  On,  Off, Off},
     // Prod: lc_creator_seed_sw_rw_en_o (On if device is not personalized),
     // lc_seed_hw_rd_en_o (On if device is personalized)
-    {Off, Off, Off, On,  On,  On,  On,  On,  On,  On,  Off},
+    {Off, Off, Off, On,  On,  On,  On,  On,  On,  On,  Off, Off},
     // ProdEnd: lc_creator_seed_sw_rw_en_o (On if device is not personalized),
     // lc_seed_hw_rd_en_o (On if device is personalized)
-    {Off, Off, Off, On,  On,  On,  On,  On,  On,  On,  Off},
+    {Off, Off, Off, On,  On,  On,  On,  On,  On,  On,  Off, Off},
     // Rma
-    {On,  On,  On,  On,  On,  On,  On,  On,  On,  On,  Off},
+    {On,  On,  On,  On,  On,  On,  On,  On,  On,  On,  Off, On},
     // Scrap
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, On},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, On, Off},
     // PostTrans
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // Escalate
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, On},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, On, Off},
     // Invalid
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, On}
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, On, Off}
   };
   // verilog_format: on
 

hw/ip/lc_ctrl/dv/env/lc_ctrl_if.sv
@@ -39,6 +39,7 @@ interface lc_ctrl_if #(
   lc_tx_t lc_iso_part_sw_rd_en_o;
   lc_tx_t lc_iso_part_sw_wr_en_o;
   lc_tx_t lc_seed_hw_rd_en_o;
+  lc_tx_t lc_rma_sw_rd_override_en_o;
   lc_tx_t lc_keymgr_en_o;
   lc_tx_t lc_escalate_en_o;
   lc_tx_t lc_check_byp_en_o;

hw/ip/lc_ctrl/rtl/lc_ctrl_signal_decode.sv
@@ -42,6 +42,7 @@ module lc_ctrl_signal_decode
   output lc_tx_t         lc_iso_part_sw_rd_en_o,
   output lc_tx_t         lc_iso_part_sw_wr_en_o,
   output lc_tx_t         lc_seed_hw_rd_en_o,
+  output lc_tx_t         lc_rma_sw_rd_override_en_o,
   output lc_tx_t         lc_keymgr_en_o,
   output lc_tx_t         lc_escalate_en_o,
   // State group diversification value for keymgr
@@ -57,6 +58,7 @@ module lc_ctrl_signal_decode
           lc_escalate_en;
   lc_tx_t lc_creator_seed_sw_rw_en, lc_owner_seed_sw_rw_en, lc_iso_part_sw_rd_en;
   lc_tx_t lc_iso_part_sw_wr_en, lc_seed_hw_rd_en;
+  lc_tx_t lc_rma_sw_rd_override_en;
   lc_keymgr_div_t lc_keymgr_div_d, lc_keymgr_div_q;
 
   always_comb begin : p_lc_signal_decode
@@ -72,6 +74,7 @@ module lc_ctrl_signal_decode
     lc_iso_part_sw_rd_en     = Off;
     lc_iso_part_sw_wr_en     = Off;
     lc_seed_hw_rd_en         = Off;
+    lc_rma_sw_rd_override_en = Off;
     lc_keymgr_en             = Off;
     // This ensures that once escalation has been triggered, it cannot go back to Off.
     lc_escalate_en           = lc_tx_or_hi(Off, lc_escalate_en_o);
@@ -189,6 +192,7 @@ module lc_ctrl_signal_decode
               lc_iso_part_sw_wr_en     = On;
               lc_iso_part_sw_rd_en     = On;
               lc_seed_hw_rd_en         = On;
+              lc_rma_sw_rd_override_en = On;
               lc_keymgr_div_d          = RndCnstLcKeymgrDivRma;
             end
             ///////////////////////////////////////////////////////////////////
@@ -300,6 +304,12 @@ module lc_ctrl_signal_decode
     .lc_en_i(lc_seed_hw_rd_en),
     .lc_en_o(lc_seed_hw_rd_en_o)
   );
+  prim_lc_sender u_prim_lc_sender_rma_sw_rd_override_en (
+    .clk_i,
+    .rst_ni,
+    .lc_en_i(lc_rma_sw_rd_override_en),
+    .lc_en_o(lc_rma_sw_rd_override_en_o)
+  );
   prim_lc_sender u_prim_lc_sender_keymgr_en (
     .clk_i,
     .rst_ni,
@@ -364,6 +374,7 @@ module lc_ctrl_signal_decode
       lc_tx_test_false_strict(lc_iso_part_sw_rd_en_o) &&
       lc_tx_test_false_strict(lc_iso_part_sw_wr_en_o) &&
       lc_tx_test_false_strict(lc_seed_hw_rd_en_o) &&
+      lc_tx_test_false_strict(lc_rma_sw_rd_override_en_o) &&
       lc_tx_test_false_strict(lc_keymgr_en_o) &&
       lc_tx_test_false_strict(lc_dft_en_o) &&
       lc_keymgr_div_o == RndCnstLcKeymgrDivInvalid)

pavona/pavona

hw/ip/lc_ctrl/dv/env/lc_ctrl_env_pkg.sv
@@ -62,6 +62,7 @@ package lc_ctrl_env_pkg;
     lc_ctrl_pkg::lc_tx_t lc_iso_part_sw_wr_en_o;
     lc_ctrl_pkg::lc_tx_t lc_keymgr_en_o;
     lc_ctrl_pkg::lc_tx_t lc_escalate_en_o;
+    lc_ctrl_pkg::lc_tx_t lc_rma_state_o;
   } lc_outputs_t;
 
   // error injection
@@ -145,56 +146,56 @@ package lc_ctrl_env_pkg;
   // verilog_format: off
   const lc_outputs_t EXP_LC_OUTPUTS[NUM_STATES] = {
     // Raw (fixed size array index 0)
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // TestUnlock0
-    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off},
+    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off, Off},
     // TestLock0
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // TestUnlock1
-    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off},
+    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off, Off},
     // TestLock1
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // TestUnlock2
-    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off},
+    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off, Off},
     // TestLock2
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // TestUnlock3
-    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off},
+    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off, Off},
     // TestLock3
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // TestUnlock4
-    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off},
+    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off, Off},
     // TestLock4
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // TestUnlock5
-    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off},
+    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off, Off},
     // TestLock5
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // TestUnlock6
-    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off},
+    {On,  On,  On,  On,  Off, Off, Off, Off, On,  Off, Off, Off},
     // TestLock6
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // TestUnlock7
-    {On,  Off, On,  On,  Off, Off, Off, Off, On,  Off, Off},
+    {On,  Off, On,  On,  Off, Off, Off, Off, On,  Off, Off, Off},
     // Dev: lc_creator_seed_sw_rw_en_o (On if device is not personalized),
     // lc_seed_hw_rd_en_o (On if device is personalized)
-    {Off, Off, On,  On,  On,  On,  On,  Off, On,  On,  Off},
+    {Off, Off, On,  On,  On,  On,  On,  Off, On,  On,  Off, Off},
     // Prod: lc_creator_seed_sw_rw_en_o (On if device is not personalized),
     // lc_seed_hw_rd_en_o (On if device is personalized)
-    {Off, Off, Off, On,  On,  On,  On,  On,  On,  On,  Off},
+    {Off, Off, Off, On,  On,  On,  On,  On,  On,  On,  Off, Off},
     // ProdEnd: lc_creator_seed_sw_rw_en_o (On if device is not personalized),
     // lc_seed_hw_rd_en_o (On if device is personalized)
-    {Off, Off, Off, On,  On,  On,  On,  On,  On,  On,  Off},
+    {Off, Off, Off, On,  On,  On,  On,  On,  On,  On,  Off, Off},
     // Rma
-    {On,  On,  On,  On,  On,  On,  On,  On,  On,  On,  Off},
+    {On,  On,  On,  On,  On,  On,  On,  On,  On,  On,  Off, On},
     // Scrap
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, On},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, On, Off},
     // PostTrans
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, Off},
     // Escalate
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, On},
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, On, Off},
     // Invalid
-    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, On}
+    {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, On, Off}
   };
   // verilog_format: on
 

hw/ip/lc_ctrl/dv/env/lc_ctrl_if.sv
@@ -39,6 +39,7 @@ interface lc_ctrl_if #(
   lc_tx_t lc_iso_part_sw_rd_en_o;
   lc_tx_t lc_iso_part_sw_wr_en_o;
   lc_tx_t lc_seed_hw_rd_en_o;
+  lc_tx_t lc_rma_state_o;
   lc_tx_t lc_keymgr_en_o;
   lc_tx_t lc_escalate_en_o;
   lc_tx_t lc_check_byp_en_o;

hw/ip/lc_ctrl/rtl/lc_ctrl_signal_decode.sv
@@ -42,6 +42,7 @@ module lc_ctrl_signal_decode
   output lc_tx_t         lc_iso_part_sw_rd_en_o,
   output lc_tx_t         lc_iso_part_sw_wr_en_o,
   output lc_tx_t         lc_seed_hw_rd_en_o,
+  output lc_tx_t         lc_rma_state_o,
   output lc_tx_t         lc_keymgr_en_o,
   output lc_tx_t         lc_escalate_en_o,
   // State group diversification value for keymgr
@@ -57,6 +58,7 @@ module lc_ctrl_signal_decode
           lc_escalate_en;
   lc_tx_t lc_creator_seed_sw_rw_en, lc_owner_seed_sw_rw_en, lc_iso_part_sw_rd_en;
   lc_tx_t lc_iso_part_sw_wr_en, lc_seed_hw_rd_en;
+  lc_tx_t lc_rma_state;
   lc_keymgr_div_t lc_keymgr_div_d, lc_keymgr_div_q;
 
   always_comb begin : p_lc_signal_decode
@@ -72,6 +74,7 @@ module lc_ctrl_signal_decode
     lc_iso_part_sw_rd_en     = Off;
     lc_iso_part_sw_wr_en     = Off;
     lc_seed_hw_rd_en         = Off;
+    lc_rma_state             = Off;
     lc_keymgr_en             = Off;
     // This ensures that once escalation has been triggered, it cannot go back to Off.
     lc_escalate_en           = lc_tx_or_hi(Off, lc_escalate_en_o);
@@ -189,6 +192,7 @@ module lc_ctrl_signal_decode
               lc_iso_part_sw_wr_en     = On;
               lc_iso_part_sw_rd_en     = On;
               lc_seed_hw_rd_en         = On;
+              lc_rma_state             = On;
               lc_keymgr_div_d          = RndCnstLcKeymgrDivRma;
             end
             ///////////////////////////////////////////////////////////////////
@@ -300,6 +304,12 @@ module lc_ctrl_signal_decode
     .lc_en_i(lc_seed_hw_rd_en),
     .lc_en_o(lc_seed_hw_rd_en_o)
   );
+  prim_lc_sender u_prim_lc_sender_rma_state (
+    .clk_i,
+    .rst_ni,
+    .lc_en_i(lc_rma_state),
+    .lc_en_o(lc_rma_state_o)
+  );
   prim_lc_sender u_prim_lc_sender_keymgr_en (
     .clk_i,
     .rst_ni,
@@ -364,6 +374,7 @@ module lc_ctrl_signal_decode
       lc_tx_test_false_strict(lc_iso_part_sw_rd_en_o) &&
       lc_tx_test_false_strict(lc_iso_part_sw_wr_en_o) &&
       lc_tx_test_false_strict(lc_seed_hw_rd_en_o) &&
+      lc_tx_test_false_strict(lc_rma_state_o) &&
       lc_tx_test_false_strict(lc_keymgr_en_o) &&
       lc_tx_test_false_strict(lc_dft_en_o) &&
       lc_keymgr_div_o == RndCnstLcKeymgrDivInvalid)