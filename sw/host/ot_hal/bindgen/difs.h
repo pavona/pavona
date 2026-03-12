zerorisc/expo

[opentitanlib] Build opentitanlib for Darjeeling
Signed-off-by: Ryan Torok <rtorok@zerorisc.com>

sw/host/ot_hal/bindgen/difs.h
@@ -19,7 +19,8 @@
 #include "hw/top/sram_ctrl_regs.h"  // Generated.
 #include "hw/top/uart_regs.h"       // Generated.
 
-#else
+#endif
+#ifdef OPENTITAN_IS_EARLGREY
 
 #include "hw/top/adc_ctrl_regs.h"       // Generated.
 #include "hw/top/aes_regs.h"            // Generated.
@@ -51,6 +52,33 @@
 #include "hw/top/sysrst_ctrl_regs.h"    // Generated.
 #include "hw/top/uart_regs.h"           // Generated.
 #include "hw/top/usbdev_regs.h"         // Generated.
+
+#endif
+#ifdef OPENTITAN_IS_DARJEELING
+
+#include "hw/top/aes_regs.h"            // Generated.
+#include "hw/top/alert_handler_regs.h"  // Generated.
+#include "hw/top/aon_timer_regs.h"      // Generated.
+#include "hw/top/clkmgr_regs.h"         // Generated.
+#include "hw/top/csrng_regs.h"          // Generated.
+#include "hw/top/edn_regs.h"            // Generated.
+#include "hw/top/entropy_src_regs.h"    // Generated.
+#include "hw/top/gpio_regs.h"           // Generated.
+#include "hw/top/hmac_regs.h"           // Generated.
+#include "hw/top/i2c_regs.h"            // Generated.
+#include "hw/top/keymgr_dpe_regs.h"     // Generated.
+#include "hw/top/kmac_regs.h"           // Generated.
+#include "hw/top/lc_ctrl_regs.h"        // Generated.
+#include "hw/top/otbn_regs.h"           // Generated.
+#include "hw/top/otp_ctrl_regs.h"       // Generated.
+#include "hw/top/pinmux_regs.h"         // Generated.
+#include "hw/top/pwrmgr_regs.h"         // Generated.
+#include "hw/top/rstmgr_regs.h"         // Generated.
+#include "hw/top/rv_timer_regs.h"       // Generated.
+#include "hw/top/spi_device_regs.h"     // Generated.
+#include "hw/top/spi_host_regs.h"       // Generated.
+#include "hw/top/sram_ctrl_regs.h"      // Generated.
+#include "hw/top/uart_regs.h"           // Generated.
 
 #endif
 

pavona/pavona

sw/host/ot_hal/bindgen/difs.h
@@ -19,7 +19,8 @@
 #include "hw/top/sram_ctrl_regs.h"  // Generated.
 #include "hw/top/uart_regs.h"       // Generated.
 
-#else
+#endif
+#ifdef OPENTITAN_IS_EARLGREY
 
 #include "hw/top/acc_regs.h"            // Generated.
 #include "hw/top/adc_ctrl_regs.h"       // Generated.
@@ -51,6 +52,33 @@
 #include "hw/top/sysrst_ctrl_regs.h"    // Generated.
 #include "hw/top/uart_regs.h"           // Generated.
 #include "hw/top/usbdev_regs.h"         // Generated.
+
+#endif
+#ifdef OPENTITAN_IS_DARJEELING
+
+#include "hw/top/acc_regs.h"            // Generated.
+#include "hw/top/aes_regs.h"            // Generated.
+#include "hw/top/alert_handler_regs.h"  // Generated.
+#include "hw/top/aon_timer_regs.h"      // Generated.
+#include "hw/top/clkmgr_regs.h"         // Generated.
+#include "hw/top/csrng_regs.h"          // Generated.
+#include "hw/top/edn_regs.h"            // Generated.
+#include "hw/top/entropy_src_regs.h"    // Generated.
+#include "hw/top/gpio_regs.h"           // Generated.
+#include "hw/top/hmac_regs.h"           // Generated.
+#include "hw/top/i2c_regs.h"            // Generated.
+#include "hw/top/keymgr_dpe_regs.h"     // Generated.
+#include "hw/top/kmac_regs.h"           // Generated.
+#include "hw/top/lc_ctrl_regs.h"        // Generated.
+#include "hw/top/otp_ctrl_regs.h"       // Generated.
+#include "hw/top/pinmux_regs.h"         // Generated.
+#include "hw/top/pwrmgr_regs.h"         // Generated.
+#include "hw/top/rstmgr_regs.h"         // Generated.
+#include "hw/top/rv_timer_regs.h"       // Generated.
+#include "hw/top/spi_device_regs.h"     // Generated.
+#include "hw/top/spi_host_regs.h"       // Generated.
+#include "hw/top/sram_ctrl_regs.h"      // Generated.
+#include "hw/top/uart_regs.h"           // Generated.
 
 #endif
 