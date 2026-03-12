lowRISC/opentitan

[prim,fpv] Add a "helper assertion" for prim_diff_decode
When doing FPV, the model checker can end up searching in an
unreachable state where it happens to have started with a differential
decoder in state 2'b11. To avoid it wasting lots of time looking at
"stuck" unreachable states, prove that this can't happen.

The "after_load" script registers this property as a helper, which
should mean state space exploration for things driven by the level of
this signal doesn't get stuck looking at unreachable states where
state_q is a constant 2'b11.

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/ip/prim/fpv/tb/prim_diff_decode_bind_fpv.sv
@@ -1,0 +1,10 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// Bind prim_diff_decode_assert_fpv into each instance of prim_diff_decode in the design.
+
+module prim_diff_decode_fpv;
+  bind prim_diff_decode prim_diff_decode_assert_fpv
+    #(.AsyncOn (AsyncOn)) u_assert_fpv (.clk_i, .rst_ni);
+endmodule

hw/ip/prim/fpv/vip/prim_diff_decode_assert_fpv.sv
@@ -1,0 +1,23 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Assertions that can be bound in to an instance of the prim_diff_decode module
+
+module prim_diff_decode_assert_fpv #(
+  parameter bit AsyncOn = 1'b0
+) (
+  input        clk_i,
+  input        rst_ni
+);
+  default clocking @(posedge clk_i); endclocking
+  default disable iff !rst_ni;
+
+  if (AsyncOn) begin : gen_async_assertions
+    // There are three valid states for the FSM and we never expect it to get to the other value in
+    // the 2-bit encoding.
+    ValidState_A: assert property (gen_async.state_q inside {gen_async.IsStd,
+                                                             gen_async.IsSkewed,
+                                                             gen_async.SigInt});
+  end
+endmodule

pavona/pavona

hw/ip/prim/fpv/tb/prim_diff_decode_bind_fpv.sv
@@ -1,0 +1,10 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// Bind prim_diff_decode_assert_fpv into each instance of prim_diff_decode in the design.
+
+module prim_diff_decode_bind_fpv;
+  bind prim_diff_decode prim_diff_decode_assert_fpv
+    #(.AsyncOn (AsyncOn)) u_assert_fpv (.clk_i, .rst_ni);
+endmodule

hw/ip/prim/fpv/vip/prim_diff_decode_assert_fpv.sv
@@ -1,0 +1,23 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Assertions that can be bound in to an instance of the prim_diff_decode module
+
+module prim_diff_decode_assert_fpv #(
+  parameter bit AsyncOn = 1'b0
+) (
+  input        clk_i,
+  input        rst_ni
+);
+  default clocking @(posedge clk_i); endclocking
+  default disable iff !rst_ni;
+
+  if (AsyncOn) begin : gen_async_assertions
+    // There are three valid states for the FSM and we never expect it to get to the other value in
+    // the 2-bit encoding.
+    ValidState_A: assert property (gen_async.state_q inside {gen_async.IsStd,
+                                                             gen_async.IsSkewing,
+                                                             gen_async.SigInt});
+  end
+endmodule