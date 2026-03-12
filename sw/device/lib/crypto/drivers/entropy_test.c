lowRISC/opentitan

[dt] Move bazel targets to the //hw/top/dt and drop the dt_ prefix
This change is simply renaming:
- //hw/top:dt_X -> //hw/top/dt:X
- #include "hw/top/dt/dt_X.h" -> "hw/top/dt/X.h"

This commit is therefore mostly automatic renaming with sed, along
with some manual fixing for autogenerated code. This since the bazel
targets are now in their own package, there is no need for dtgen to
output files to a `dt/` directory.

Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

sw/device/lib/crypto/drivers/entropy_test.c
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 #include "sw/device/lib/crypto/drivers/entropy.h"
 
-#include "hw/top/dt/dt_otbn.h"
+#include "hw/top/dt/otbn.h"
 #include "sw/device/lib/base/memory.h"
 #include "sw/device/lib/base/status.h"
 #include "sw/device/lib/crypto/drivers/entropy_kat.h"

pavona/pavona

sw/device/lib/crypto/drivers/entropy_test.c
@@ -4,7 +4,7 @@
 // SPDX-License-Identifier: Apache-2.0
 #include "sw/device/lib/crypto/drivers/entropy.h"
 
-#include "hw/top/dt/dt_acc.h"
+#include "hw/top/dt/acc.h"
 #include "sw/device/lib/base/memory.h"
 #include "sw/device/lib/base/status.h"
 #include "sw/device/lib/crypto/drivers/entropy_kat.h"