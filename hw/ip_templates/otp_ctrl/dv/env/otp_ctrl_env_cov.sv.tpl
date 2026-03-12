lowRISC/opentitan

[otp_ctrl,dv] Pass some constructor arguments more explicitly
This is motivated by the fact that I want to get rid of the middle
optional argument for bit_toggle_cg_wrap. There is only non-trivial
use of the argument in the repository and it is the creation of
status_csr_cg in this module.

Inline the calculation here instead, and name the toggle_cov_en
argument, which lets us not explicitly provide the middle argument.

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/ip_templates/otp_ctrl/dv/env/otp_ctrl_env_cov.sv.tpl
@@ -293,11 +293,11 @@ class otp_ctrl_env_cov extends cip_base_env_cov #(.CFG_T(otp_ctrl_env_cfg));
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     // Create instances from bit_toggle_cg_wrapper.
-    lc_prog_cg  = new("lc_prog_cg", "", 0);
-    otbn_req_cg = new("otbn_req_cg", "", 0);
+    lc_prog_cg  = new("lc_prog_cg", .toggle_cov_en(0));
+    otbn_req_cg = new("otbn_req_cg", .toggle_cov_en(0));
     foreach (status_csr_cg[i]) begin
       otp_status_e index = otp_status_e'(i);
-      status_csr_cg[i]= new(index.name, "status_csr_cg", 0);
+      status_csr_cg[i]= new({"status_csr_cg::", index.name}, .toggle_cov_en(0));
     end
 
     // Create instances from external wrapper classes.

hw/top_darjeeling/ip_autogen/otp_ctrl/dv/env/otp_ctrl_env_cov.sv
@@ -335,11 +335,11 @@ class otp_ctrl_env_cov extends cip_base_env_cov #(.CFG_T(otp_ctrl_env_cfg));
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     // Create instances from bit_toggle_cg_wrapper.
-    lc_prog_cg  = new("lc_prog_cg", "", 0);
-    otbn_req_cg = new("otbn_req_cg", "", 0);
+    lc_prog_cg  = new("lc_prog_cg", .toggle_cov_en(0));
+    otbn_req_cg = new("otbn_req_cg", .toggle_cov_en(0));
     foreach (status_csr_cg[i]) begin
       otp_status_e index = otp_status_e'(i);
-      status_csr_cg[i]= new(index.name, "status_csr_cg", 0);
+      status_csr_cg[i]= new({"status_csr_cg::", index.name}, .toggle_cov_en(0));
     end
 
     // Create instances from external wrapper classes.

hw/top_earlgrey/ip_autogen/otp_ctrl/dv/env/otp_ctrl_env_cov.sv
@@ -296,11 +296,11 @@ class otp_ctrl_env_cov extends cip_base_env_cov #(.CFG_T(otp_ctrl_env_cfg));
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     // Create instances from bit_toggle_cg_wrapper.
-    lc_prog_cg  = new("lc_prog_cg", "", 0);
-    otbn_req_cg = new("otbn_req_cg", "", 0);
+    lc_prog_cg  = new("lc_prog_cg", .toggle_cov_en(0));
+    otbn_req_cg = new("otbn_req_cg", .toggle_cov_en(0));
     foreach (status_csr_cg[i]) begin
       otp_status_e index = otp_status_e'(i);
-      status_csr_cg[i]= new(index.name, "status_csr_cg", 0);
+      status_csr_cg[i]= new({"status_csr_cg::", index.name}, .toggle_cov_en(0));
     end
 
     // Create instances from external wrapper classes.

pavona/pavona

hw/ip_templates/otp_ctrl/dv/env/otp_ctrl_env_cov.sv.tpl
@@ -416,11 +416,11 @@ class otp_ctrl_env_cov extends cip_base_env_cov #(.CFG_T(otp_ctrl_env_cfg));
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     // Create instances from bit_toggle_cg_wrapper.
-    lc_prog_cg  = new("lc_prog_cg", "", 0);
-    acc_req_cg = new("acc_req_cg", "", 0);
+    lc_prog_cg  = new("lc_prog_cg", .toggle_cov_en(0));
+    acc_req_cg = new("acc_req_cg", .toggle_cov_en(0));
     foreach (status_csr_cg[i]) begin
       otp_status_e index = otp_status_e'(i);
-      status_csr_cg[i]= new(index.name, "status_csr_cg", 0);
+      status_csr_cg[i]= new({"status_csr_cg::", index.name}, .toggle_cov_en(0));
     end
 
     // Create instances from external wrapper classes.

hw/top_darjeeling/ip_autogen/otp_ctrl/dv/env/otp_ctrl_env_cov.sv
@@ -492,11 +492,11 @@ class otp_ctrl_env_cov extends cip_base_env_cov #(.CFG_T(otp_ctrl_env_cfg));
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     // Create instances from bit_toggle_cg_wrapper.
-    lc_prog_cg  = new("lc_prog_cg", "", 0);
-    acc_req_cg = new("acc_req_cg", "", 0);
+    lc_prog_cg  = new("lc_prog_cg", .toggle_cov_en(0));
+    acc_req_cg = new("acc_req_cg", .toggle_cov_en(0));
     foreach (status_csr_cg[i]) begin
       otp_status_e index = otp_status_e'(i);
-      status_csr_cg[i]= new(index.name, "status_csr_cg", 0);
+      status_csr_cg[i]= new({"status_csr_cg::", index.name}, .toggle_cov_en(0));
     end
 
     // Create instances from external wrapper classes.

hw/top_earlgrey/ip_autogen/otp_ctrl/dv/env/otp_ctrl_env_cov.sv
@@ -425,11 +425,11 @@ class otp_ctrl_env_cov extends cip_base_env_cov #(.CFG_T(otp_ctrl_env_cfg));
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     // Create instances from bit_toggle_cg_wrapper.
-    lc_prog_cg  = new("lc_prog_cg", "", 0);
-    acc_req_cg = new("acc_req_cg", "", 0);
+    lc_prog_cg  = new("lc_prog_cg", .toggle_cov_en(0));
+    acc_req_cg = new("acc_req_cg", .toggle_cov_en(0));
     foreach (status_csr_cg[i]) begin
       otp_status_e index = otp_status_e'(i);
-      status_csr_cg[i]= new(index.name, "status_csr_cg", 0);
+      status_csr_cg[i]= new({"status_csr_cg::", index.name}, .toggle_cov_en(0));
     end
 
     // Create instances from external wrapper classes.