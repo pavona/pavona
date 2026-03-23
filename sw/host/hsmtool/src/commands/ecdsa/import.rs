lowRISC/opentitan

[hsmtool] Add vendor specific AES_KWP.
The HSM used in provisioning infrastructure uses a custom mechanism
identifier (`CKM_AES_KWP = (CKM_VENDOR_DEFINED + 0x171)`) even though
the implementation follows the RFC 3394 and 5649 specifations.

The `CKM_AES_KWP` implemented by Thales is also equivalent to the KWP
algorithm specified by NIST SP 800-38F.

This change adds a custom `Wrap::VendorThalesAesKwp` mechanism to
`hsmtool` to be able to wrap/unwrap private keys with `AES_KWP`.

Signed-off-by: Miguel Osorio <miguelosorio@google.com>

sw/host/hsmtool/src/commands/ecdsa/import.rs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use cryptoki::session::Session;
 use serde::{Deserialize, Serialize};
 use std::any::Any;
@@ -15,7 +15,7 @@ use crate::module::Module;
 use crate::util::attribute::{AttrData, AttributeMap, AttributeType};
 use crate::util::helper;
 use crate::util::key::ecdsa::{load_private_key, load_public_key};
-use crate::util::wrap::Wrap;
+use crate::util::wrap::{Wrap, WrapPrivateKey};
 
 #[derive(clap::Args, Debug, Serialize, Deserialize)]
 pub struct Import {
@@ -35,6 +35,9 @@ pub struct Import {
     /// Unwrap the imported key with a wrapping key.
     #[arg(long)]
     unwrap: Option<String>,
+    /// Unwrapping key mechanism. Required when unwrap is specified.
+    #[arg(long, default_value = "aes-key-wrap-pad")]
+    unwrap_mechanism: Option<WrapPrivateKey>,
     filename: PathBuf,
 }
 
@@ -57,7 +60,13 @@ impl Import {
 
     fn unwrap_key(&self, session: &Session, template: &AttributeMap) -> Result<()> {
         let key = helper::read_file(&self.filename)?;
-        let wrapper = Wrap::AesKeyWrapPad;
+        let wrapper: Wrap = self
+            .unwrap_mechanism
+            .as_ref()
+            .ok_or(anyhow!(
+                "unwrap_mechanism is required when wrap is specified"
+            ))?
+            .into();
         let _key = wrapper.unwrap(session, key.as_slice(), self.unwrap.as_deref(), template)?;
         Ok(())
     }

sw/host/hsmtool/src/commands/rsa/import.rs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use cryptoki::session::Session;
 use serde::{Deserialize, Serialize};
 use std::any::Any;
@@ -15,7 +15,7 @@ use crate::module::Module;
 use crate::util::attribute::{AttrData, AttributeMap, AttributeType};
 use crate::util::helper;
 use crate::util::key::rsa::{load_private_key, load_public_key};
-use crate::util::wrap::Wrap;
+use crate::util::wrap::{Wrap, WrapPrivateKey};
 
 #[derive(clap::Args, Debug, Serialize, Deserialize)]
 pub struct Import {
@@ -35,6 +35,9 @@ pub struct Import {
     /// Unwrap the imported key with a wrapping key.
     #[arg(long)]
     unwrap: Option<String>,
+    /// Unwrapping key mechanism. Required when unwrap is specified.
+    #[arg(long, default_value = "aes-key-wrap-pad")]
+    unwrap_mechanism: Option<WrapPrivateKey>,
     filename: PathBuf,
 }
 
@@ -59,7 +62,13 @@ impl Import {
 
     fn unwrap_key(&self, session: &Session, template: &AttributeMap) -> Result<()> {
         let key = helper::read_file(&self.filename)?;
-        let wrapper = Wrap::AesKeyWrapPad;
+        let wrapper: Wrap = self
+            .unwrap_mechanism
+            .as_ref()
+            .ok_or(anyhow!(
+                "unwrap_mechanism is required when wrap is specified"
+            ))?
+            .into();
         let _key = wrapper.unwrap(session, key.as_slice(), self.unwrap.as_deref(), template)?;
         Ok(())
     }

pavona/pavona

(cherry picked from commit a1feef4be99daa688394d4d63c6a0c751d3193d8)

sw/host/hsmtool/src/commands/ecdsa/import.rs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-use anyhow::Result;
+use anyhow::{Result, anyhow};
 use cryptoki::session::Session;
 use serde::{Deserialize, Serialize};
 use std::any::Any;
@@ -15,7 +15,7 @@ use crate::module::Module;
 use crate::util::attribute::{AttrData, AttributeMap, AttributeType};
 use crate::util::helper;
 use crate::util::key::ecdsa::{load_private_key, load_public_key};
-use crate::util::wrap::Wrap;
+use crate::util::wrap::{Wrap, WrapPrivateKey};
 
 #[derive(clap::Args, Debug, Serialize, Deserialize)]
 pub struct Import {
@@ -35,6 +35,9 @@ pub struct Import {
     /// Unwrap the imported key with a wrapping key.
     #[arg(long)]
     unwrap: Option<String>,
+    /// Unwrapping key mechanism. Required when unwrap is specified.
+    #[arg(long, default_value = "aes-key-wrap-pad")]
+    unwrap_mechanism: Option<WrapPrivateKey>,
     filename: PathBuf,
 }
 
@@ -57,7 +60,12 @@ impl Import {
 
     fn unwrap_key(&self, session: &Session, template: &AttributeMap) -> Result<()> {
         let key = std::fs::read(&self.filename)?;
-        let wrapper = Wrap::AesKeyWrapPad;
+        let wrapper: Wrap = self
+            .unwrap_mechanism
+            .ok_or(anyhow!(
+                "unwrap_mechanism is required when wrap is specified"
+            ))?
+            .into();
         let _key = wrapper.unwrap(session, key.as_slice(), self.unwrap.as_deref(), template)?;
         Ok(())
     }

sw/host/hsmtool/src/commands/rsa/import.rs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-use anyhow::Result;
+use anyhow::{Result, anyhow};
 use cryptoki::session::Session;
 use serde::{Deserialize, Serialize};
 use std::any::Any;
@@ -15,7 +15,7 @@ use crate::module::Module;
 use crate::util::attribute::{AttrData, AttributeMap, AttributeType};
 use crate::util::helper;
 use crate::util::key::rsa::{load_private_key, load_public_key};
-use crate::util::wrap::Wrap;
+use crate::util::wrap::{Wrap, WrapPrivateKey};
 
 #[derive(clap::Args, Debug, Serialize, Deserialize)]
 pub struct Import {
@@ -35,6 +35,9 @@ pub struct Import {
     /// Unwrap the imported key with a wrapping key.
     #[arg(long)]
     unwrap: Option<String>,
+    /// Unwrapping key mechanism. Required when unwrap is specified.
+    #[arg(long, default_value = "aes-key-wrap-pad")]
+    unwrap_mechanism: Option<WrapPrivateKey>,
     filename: PathBuf,
 }
 
@@ -59,7 +62,12 @@ impl Import {
 
     fn unwrap_key(&self, session: &Session, template: &AttributeMap) -> Result<()> {
         let key = std::fs::read(&self.filename)?;
-        let wrapper = Wrap::AesKeyWrapPad;
+        let wrapper: Wrap = self
+            .unwrap_mechanism
+            .ok_or(anyhow!(
+                "unwrap_mechanism is required when wrap is specified"
+            ))?
+            .into();
         let _key = wrapper.unwrap(session, key.as_slice(), self.unwrap.as_deref(), template)?;
         Ok(())
     }