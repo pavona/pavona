lowRISC/opentitan

[individualize] remove OTTF console init and all prints
The OTTF console initialization requires a host--device handshake when
using the SPI transport. This causes issues with ATE integration.
This removes the OTTF console initialization, and all remaining
`LOG_INFO()`s from the individualization firmware.

Signed-off-by: Tim Trippel <ttrippel@google.com>

sw/host/provisioning/ft_lib/src/lib.rs
@@ -6,22 +6,19 @@ use sha2::{Digest, Sha256};
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::path::PathBuf;
-use std::thread;
 use std::time::{Duration, Instant};
 
-use anyhow::{anyhow, bail, Result};
+use anyhow::{bail, Result};
 use arrayvec::ArrayVec;
 use zerocopy::AsBytes;
 
+use bindgen::sram_program::SRAM_MAGIC_SP_EXECUTION_DONE;
 use cert_lib::{parse_and_endorse_x509_cert, validate_cert_chain, CaConfig, CaKey, EndorsedCert};
 use ft_ext_lib::ft_ext;
 use opentitanlib::app::TransportWrapper;
 use opentitanlib::console::spi::SpiConsoleDevice;
 use opentitanlib::dif::lc_ctrl::{DifLcCtrlState, LcCtrlReg};
 use opentitanlib::io::jtag::{JtagParams, JtagTap};
-use opentitanlib::test_utils::crashdump::{
-    read_alert_crashdump_data, read_cpu_crashdump_data, read_reset_reason,
-};
 use opentitanlib::test_utils::init::InitializeTest;
 use opentitanlib::test_utils::lc_transition::trigger_lc_transition;
 use opentitanlib::test_utils::load_sram_program::{
@@ -80,14 +77,12 @@ pub fn test_unlock(
     Ok(())
 }
 
-#[allow(clippy::too_many_arguments)]
 pub fn run_sram_ft_individualize(
     transport: &TransportWrapper,
     jtag_params: &JtagParams,
     reset_delay: Duration,
     sram_program: &SramProgramParams,
     timeout: Duration,
-    spi_console: &SpiConsoleDevice,
 ) -> Result<()> {
     // Set CPU TAP straps, reset, and connect to the JTAG interface.
     transport.pin_strapping("PINMUX_TAP_RISCV")?.apply()?;
@@ -99,9 +94,12 @@ pub fn run_sram_ft_individualize(
     jtag.reset(/*run=*/ false)?;
 
     // Load and execute the SRAM program that contains the provisioning code.
-    let result = sram_program.load_and_execute(&mut *jtag, ExecutionMode::Jump)?;
+    let result = sram_program.load_and_execute(&mut *jtag, ExecutionMode::JumpAndWait(timeout))?;
     match result {
-        ExecutionResult::Executing => log::info!("SRAM program loaded and is executing."),
+        ExecutionResult::ExecutionDone(sp_val) => match sp_val {
+            SRAM_MAGIC_SP_EXECUTION_DONE => log::info!("SRAM program completed."),
+            _ => panic!("SRAM program load/execution failed: sp = {:?}.", sp_val),
+        },
         _ => panic!("SRAM program load/execution failed: {:?}.", result),
     }
 
@@ -111,24 +109,7 @@ pub fn run_sram_ft_individualize(
     transport.pin_strapping("PINMUX_TAP_RISCV")?.remove()?;
     transport.pin_strapping("PINMUX_TAP_LC")?.apply()?;
 
-    // Wait for provisioning operations to complete. If we see at least 10 NMIs, we know it is time
-    // to capture crashdump information.
-    let console_text = UartConsole::wait_for(
-        spi_console,
-        r"FT SRAM provisioning done.|Processing Alert NMI 10 ...",
-        timeout,
-    )?;
-    match console_text[0].as_str() {
-        "FT SRAM provisioning done." => Ok(()),
-        "Processing Alert NMI 10 ..." => {
-            thread::sleep(Duration::from_millis(10000));
-            read_reset_reason(transport, jtag_params)?;
-            read_cpu_crashdump_data(transport, jtag_params)?;
-            read_alert_crashdump_data(transport, jtag_params)?;
-            Ok(())
-        }
-        _ => Err(anyhow!("Unexpected console_text: {:?}", console_text)),
-    }
+    Ok(())
 }
 
 pub fn test_exit(

pavona/pavona

(cherry picked from commit 97bae45711d3bb2b1146bd240ee70cd94d2bbdbc)

sw/host/provisioning/ft_lib/src/lib.rs
@@ -7,22 +7,19 @@ use sha2::{Digest, Sha256};
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::path::PathBuf;
-use std::thread;
 use std::time::{Duration, Instant};
 
-use anyhow::{Result, anyhow, bail};
+use anyhow::{Result, bail};
 use arrayvec::ArrayVec;
 use zerocopy::IntoBytes;
 
+use bindgen::sram_program::SRAM_MAGIC_SP_EXECUTION_DONE;
 use cert_lib::{CaConfig, CaKey, EndorsedCert, parse_and_endorse_x509_cert, validate_cert_chain};
 use ft_ext_lib::ft_ext;
 use opentitanlib::app::{TransportWrapper, UartRx};
 use opentitanlib::console::spi::SpiConsoleDevice;
 use opentitanlib::io::console::ConsoleError;
 use opentitanlib::io::jtag::{JtagParams, JtagTap};
-use opentitanlib::test_utils::crashdump::{
-    read_alert_crashdump_data, read_cpu_crashdump_data, read_reset_reason,
-};
 use opentitanlib::test_utils::init::InitializeTest;
 use opentitanlib::test_utils::lc_transition::trigger_lc_transition;
 use opentitanlib::test_utils::load_sram_program::{
@@ -41,10 +38,6 @@ use util_lib::hash_lc_token;
 
 pub mod response;
 use response::*;
-
-// After seeing 10 alert NMIs, this is the time to wait after disconnecting JTAG
-// before capturing crashdump information.
-const FT_NMI_CRASHDUMP_DELAY_MILLIS: u64 = 10000; // 10 seconds
 
 pub fn test_unlock(
     transport: &TransportWrapper,
@@ -84,13 +77,11 @@ pub fn test_unlock(
     Ok(())
 }
 
-#[allow(clippy::too_many_arguments)]
 pub fn run_sram_ft_individualize(
     transport: &TransportWrapper,
     jtag_params: &JtagParams,
     sram_program: &SramProgramParams,
     timeout: Duration,
-    spi_console: &SpiConsoleDevice,
 ) -> Result<()> {
     // Set CPU TAP straps, reset, and connect to the JTAG interface.
     transport.pin_strapping("PINMUX_TAP_RISCV")?.apply()?;
@@ -102,9 +93,12 @@ pub fn run_sram_ft_individualize(
     jtag.reset(/*run=*/ false)?;
 
     // Load and execute the SRAM program that contains the provisioning code.
-    let result = sram_program.load_and_execute(&mut *jtag, ExecutionMode::Jump)?;
+    let result = sram_program.load_and_execute(&mut *jtag, ExecutionMode::JumpAndWait(timeout))?;
     match result {
-        ExecutionResult::Executing => log::info!("SRAM program loaded and is executing."),
+        ExecutionResult::ExecutionDone(sp_val) => match sp_val {
+            SRAM_MAGIC_SP_EXECUTION_DONE => log::info!("SRAM program completed."),
+            _ => panic!("SRAM program load/execution failed: sp = {:?}.", sp_val),
+        },
         _ => panic!("SRAM program load/execution failed: {:?}.", result),
     }
 
@@ -114,28 +108,7 @@ pub fn run_sram_ft_individualize(
     transport.pin_strapping("PINMUX_TAP_RISCV")?.remove()?;
     transport.pin_strapping("PINMUX_TAP_LC")?.apply()?;
 
-    // Wait for provisioning operations to complete. If we see at least 10 NMIs, we know it is time
-    // to capture crashdump information.
-    let console_text = UartConsole::wait_for(
-        spi_console,
-        r"FT SRAM provisioning done.|Processing Alert NMI 10 ...",
-        timeout,
-    )?;
-    match console_text[0].as_str() {
-        "FT SRAM provisioning done." => Ok(()),
-        "Processing Alert NMI 10 ..." => {
-            log::info!(
-                "10 NMIs detected, waiting {:?} before capturing crashdump information",
-                Duration::from_millis(FT_NMI_CRASHDUMP_DELAY_MILLIS)
-            );
-            thread::sleep(Duration::from_millis(FT_NMI_CRASHDUMP_DELAY_MILLIS));
-            read_reset_reason(transport, jtag_params)?;
-            read_cpu_crashdump_data(transport, jtag_params)?;
-            read_alert_crashdump_data(transport, jtag_params)?;
-            Ok(())
-        }
-        _ => Err(anyhow!("Unexpected console_text: {:?}", console_text)),
-    }
+    Ok(())
 }
 
 pub fn test_exit(