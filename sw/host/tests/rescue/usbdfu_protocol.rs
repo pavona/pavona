lowRISC/opentitan

[rescue] Test rescue with standard utilities
1. Test the xmodem rescue protocol with the `sx`/`rx` programs from the
   `lrzsz` package.
2. Test the DFU rescue protocol with the `dfu-util` program from the
   `dfu-util` package.

Signed-off-by: Chris Frantz <cfrantz@google.com>

sw/host/tests/rescue/usbdfu_protocol.rs
@@ -1,0 +1,129 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#![allow(clippy::bool_assert_comparison)]
+use anyhow::{anyhow, Result};
+use clap::Parser;
+use std::process::Command;
+use std::time::Duration;
+
+use opentitanlib::app::TransportWrapper;
+use opentitanlib::chip::boot_log::BootLog;
+use opentitanlib::execute_test;
+use opentitanlib::rescue::{EntryMode, RescueParams, RescueProtocol, RescueTrigger};
+use opentitanlib::test_utils::init::InitializeTest;
+use opentitanlib::uart::console::UartConsole;
+
+#[derive(Debug, Parser)]
+struct Opts {
+    #[command(flatten)]
+    init: InitializeTest,
+
+    /// The rescue protocol.
+    #[arg(short, long, value_enum, default_value_t = RescueProtocol::UsbDfu)]
+    rescue_protocol: RescueProtocol,
+    /// The rescue trigger mechanism.
+    #[arg(short, long, value_enum, default_value_t = RescueTrigger::Strap)]
+    trigger: RescueTrigger,
+    /// The rescue trigger value.
+    #[arg(short, long, default_value = "")]
+    value: String,
+
+    /// Console receive timeout.
+    #[arg(long, value_parser = humantime::parse_duration, default_value = "10s")]
+    timeout: Duration,
+
+    /// Firmware image
+    #[arg(long)]
+    firmware: String,
+}
+
+/// Checks that we can use the primitive xmodem tools from the `lrzsz` package
+/// to perform firmware rescue.
+fn firmware_update_test(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
+    let uart = transport.uart("console")?;
+    let params = RescueParams {
+        protocol: opts.rescue_protocol,
+        trigger: opts.trigger,
+        value: opts.value.clone(),
+        ..Default::default()
+    };
+    let rescue = params.create(transport)?;
+
+    // It would be even cooler to do this in a python script or shell script, but
+    // opentitanlib is just too convenient for manipulating our test infrastructure.
+    // We trigger rescue and then use `dfu-util` to perform the actual operation.
+    rescue.enter(transport, EntryMode::Reset)?;
+    // Note: we drop the rescue instance to release our claim on the usb-dfu
+    // interface.  This allows `dfu-util` to claim and use the interface.
+    drop(rescue);
+
+    let mut child = Command::new("dfu-util")
+        .arg("--device=18d1:023a")
+        // AltSetting 0 is the firmware rescue setting.
+        .arg("--alt=0")
+        .arg("--reset")
+        .arg("--download")
+        .arg(&opts.firmware)
+        .spawn()?;
+
+    let status = child.wait()?;
+    log::info!("Got dfu-util exit code {status:?}");
+
+    let capture = UartConsole::wait_for(&*uart, r"PASS!$|BFV:([0-9A-Fa-f]{8})", opts.timeout)?;
+    if capture[0].starts_with("BFV") {
+        Err(anyhow!("Error: {}", capture[0]))
+    } else {
+        Ok(())
+    }
+}
+
+/// Checks that we can use the primitive xmodem tools from the `lrzsz` package
+/// to perform get the BootLog from the chip.
+fn get_boot_log_test(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
+    let params = RescueParams {
+        protocol: opts.rescue_protocol,
+        trigger: opts.trigger,
+        value: opts.value.clone(),
+        ..Default::default()
+    };
+    let rescue = params.create(transport)?;
+
+    let path = format!("boot_log-{}.bin", std::process::id());
+    let _ = std::fs::remove_file(&path);
+    log::info!("Receving boot_log into {path:?}");
+
+    // It would be even cooler to do this in a python script or shell script, but
+    // opentitanlib is just too convenient for manipulating our test infrastructure.
+    // We trigger rescue and then use `dfu-util` to perform the actual operation.
+    rescue.enter(transport, EntryMode::Reset)?;
+    // Note: we drop the rescue instance to release our claim on the usb-dfu
+    // interface.  This allows `dfu-util` to claim and use the interface.
+    drop(rescue);
+
+    let mut child = Command::new("dfu-util")
+        .arg("--device=18d1:023a")
+        // AltSetting 3 is the BootLog setting.
+        .arg("--alt=3")
+        .arg("--upload")
+        .arg(&path)
+        .spawn()?;
+
+    let status = child.wait()?;
+    log::info!("Got dfu-util exit code {status:?}");
+
+    let data = std::fs::read(&path)?;
+    let blog = BootLog::try_from(data.as_slice())?;
+    log::info!("BootLog = {blog:?}");
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let opts = Opts::parse();
+    opts.init.init_logging();
+    let transport = opts.init.init_target()?;
+    execute_test!(firmware_update_test, &opts, &transport);
+    execute_test!(get_boot_log_test, &opts, &transport);
+    Ok(())
+}

sw/host/tests/rescue/xmodem_protocol.rs
@@ -1,0 +1,110 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#![allow(clippy::bool_assert_comparison)]
+use anyhow::{anyhow, Result};
+use clap::Parser;
+use std::process::{Command, Stdio};
+use std::rc::Rc;
+use std::time::Duration;
+
+use opentitanlib::app::TransportWrapper;
+use opentitanlib::chip::boot_log::BootLog;
+use opentitanlib::execute_test;
+use opentitanlib::rescue::serial::RescueSerial;
+use opentitanlib::rescue::{EntryMode, Rescue, RescueMode};
+use opentitanlib::test_utils::init::InitializeTest;
+use opentitanlib::uart::console::UartConsole;
+
+#[derive(Debug, Parser)]
+struct Opts {
+    #[command(flatten)]
+    init: InitializeTest,
+
+    /// Console receive timeout.
+    #[arg(long, value_parser = humantime::parse_duration, default_value = "10s")]
+    timeout: Duration,
+
+    /// Firmware image
+    #[arg(long)]
+    firmware: String,
+}
+
+/// Checks that we can use the primitive xmodem tools from the `lrzsz` package
+/// to perform firmware rescue.
+fn firmware_update_test(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
+    let uart = transport.uart("console")?;
+    let rescue = RescueSerial::new(Rc::clone(&uart));
+
+    // It would be even cooler to do this in a python script or shell script, but
+    // opentitanlib is just too convenient for manipulating our test infrastructure.
+    // Nevertheless, we farm out the actual work to the `sx` (send xmodem) command.
+    rescue.enter(transport, EntryMode::Reset)?;
+
+    // Get the UART file descriptor and dup it to use as the child's stdin/stdout.
+    let fd = uart.borrow_fd()?;
+    let stdin = Stdio::from(rustix::io::dup(fd)?);
+    let stdout = Stdio::from(rustix::io::dup(fd)?);
+    let mut child = Command::new("sx")
+        .arg("--1k")
+        .arg(&opts.firmware)
+        .stdin(stdin)
+        .stdout(stdout)
+        .spawn()?;
+
+    let status = child.wait()?;
+    log::info!("Got xmodem exit code {status:?}");
+    rescue.reboot()?;
+
+    let capture = UartConsole::wait_for(&*uart, r"PASS!$|BFV:([0-9A-Fa-f]{8})", opts.timeout)?;
+    if capture[0].starts_with("BFV") {
+        Err(anyhow!("Error: {}", capture[0]))
+    } else {
+        Ok(())
+    }
+}
+
+/// Checks that we can use the primitive xmodem tools from the `lrzsz` package
+/// to perform get the BootLog from the chip.
+fn get_boot_log_test(_opts: &Opts, transport: &TransportWrapper) -> Result<()> {
+    let uart = transport.uart("console")?;
+    let rescue = RescueSerial::new(Rc::clone(&uart));
+
+    // It would be even cooler to do this in a python script or shell script, but
+    // opentitanlib is just too convenient for manipulating our test infrastructure.
+    // Nevertheless, we farm out the actual work to the `rx` (recv xmodem) command.
+    rescue.enter(transport, EntryMode::Reset)?;
+    rescue.set_mode(RescueMode::BootLog)?;
+
+    let path = format!("boot_log-{}.bin", std::process::id());
+    let _ = std::fs::remove_file(&path);
+    log::info!("Receving boot_log into {path:?}");
+
+    // Get the UART file descriptor and dup it to use as the child's stdin/stdout.
+    let fd = uart.borrow_fd()?;
+    let stdin = Stdio::from(rustix::io::dup(fd)?);
+    let stdout = Stdio::from(rustix::io::dup(fd)?);
+    let mut child = Command::new("rx")
+        .arg("--with-crc")
+        .arg(&path)
+        .stdin(stdin)
+        .stdout(stdout)
+        .spawn()?;
+
+    let status = child.wait()?;
+    log::info!("Got xmodem exit code {status:?}");
+    let data = std::fs::read(&path)?;
+    let blog = BootLog::try_from(data.as_slice())?;
+    log::info!("BootLog = {blog:?}");
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let opts = Opts::parse();
+    opts.init.init_logging();
+    let transport = opts.init.init_target()?;
+    execute_test!(firmware_update_test, &opts, &transport);
+    execute_test!(get_boot_log_test, &opts, &transport);
+    Ok(())
+}

pavona/pavona

(cherry picked from commit 80caf262e8d1ff021b2dfc8ac13379157d2d253c)

sw/host/tests/rescue/usbdfu_protocol.rs
@@ -1,0 +1,129 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#![allow(clippy::bool_assert_comparison)]
+use anyhow::{Result, anyhow};
+use clap::Parser;
+use std::process::Command;
+use std::time::Duration;
+
+use opentitanlib::app::TransportWrapper;
+use opentitanlib::chip::boot_log::BootLog;
+use opentitanlib::execute_test;
+use opentitanlib::rescue::{EntryMode, RescueParams, RescueProtocol, RescueTrigger};
+use opentitanlib::test_utils::init::InitializeTest;
+use opentitanlib::uart::console::UartConsole;
+
+#[derive(Debug, Parser)]
+struct Opts {
+    #[command(flatten)]
+    init: InitializeTest,
+
+    /// The rescue protocol.
+    #[arg(short, long, value_enum, default_value_t = RescueProtocol::UsbDfu)]
+    rescue_protocol: RescueProtocol,
+    /// The rescue trigger mechanism.
+    #[arg(short, long, value_enum, default_value_t = RescueTrigger::Strap)]
+    trigger: RescueTrigger,
+    /// The rescue trigger value.
+    #[arg(short, long, default_value = "")]
+    value: String,
+
+    /// Console receive timeout.
+    #[arg(long, value_parser = humantime::parse_duration, default_value = "10s")]
+    timeout: Duration,
+
+    /// Firmware image
+    #[arg(long)]
+    firmware: String,
+}
+
+/// Checks that we can use the primitive xmodem tools from the `lrzsz` package
+/// to perform firmware rescue.
+fn firmware_update_test(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
+    let uart = transport.uart("console")?;
+    let params = RescueParams {
+        protocol: opts.rescue_protocol,
+        trigger: opts.trigger,
+        value: opts.value.clone(),
+        ..Default::default()
+    };
+    let rescue = params.create(transport)?;
+
+    // It would be even cooler to do this in a python script or shell script, but
+    // opentitanlib is just too convenient for manipulating our test infrastructure.
+    // We trigger rescue and then use `dfu-util` to perform the actual operation.
+    rescue.enter(transport, EntryMode::Reset)?;
+    // Note: we drop the rescue instance to release our claim on the usb-dfu
+    // interface.  This allows `dfu-util` to claim and use the interface.
+    drop(rescue);
+
+    let mut child = Command::new("dfu-util")
+        .arg("--device=18d1:023a")
+        // AltSetting 0 is the firmware rescue setting.
+        .arg("--alt=0")
+        .arg("--reset")
+        .arg("--download")
+        .arg(&opts.firmware)
+        .spawn()?;
+
+    let status = child.wait()?;
+    log::info!("Got dfu-util exit code {status:?}");
+
+    let capture = UartConsole::wait_for(&*uart, r"PASS!|BFV:([0-9A-Fa-f]{8})", opts.timeout)?;
+    if capture[0].starts_with("BFV") {
+        Err(anyhow!("Error: {}", capture[0]))
+    } else {
+        Ok(())
+    }
+}
+
+/// Checks that we can use the primitive xmodem tools from the `lrzsz` package
+/// to perform get the BootLog from the chip.
+fn get_boot_log_test(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
+    let params = RescueParams {
+        protocol: opts.rescue_protocol,
+        trigger: opts.trigger,
+        value: opts.value.clone(),
+        ..Default::default()
+    };
+    let rescue = params.create(transport)?;
+
+    let path = format!("boot_log-{}.bin", std::process::id());
+    let _ = std::fs::remove_file(&path);
+    log::info!("Receving boot_log into {path:?}");
+
+    // It would be even cooler to do this in a python script or shell script, but
+    // opentitanlib is just too convenient for manipulating our test infrastructure.
+    // We trigger rescue and then use `dfu-util` to perform the actual operation.
+    rescue.enter(transport, EntryMode::Reset)?;
+    // Note: we drop the rescue instance to release our claim on the usb-dfu
+    // interface.  This allows `dfu-util` to claim and use the interface.
+    drop(rescue);
+
+    let mut child = Command::new("dfu-util")
+        .arg("--device=18d1:023a")
+        // AltSetting 3 is the BootLog setting.
+        .arg("--alt=3")
+        .arg("--upload")
+        .arg(&path)
+        .spawn()?;
+
+    let status = child.wait()?;
+    log::info!("Got dfu-util exit code {status:?}");
+
+    let data = std::fs::read(&path)?;
+    let blog = BootLog::try_from(data.as_slice())?;
+    log::info!("BootLog = {blog:?}");
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let opts = Opts::parse();
+    opts.init.init_logging();
+    let transport = opts.init.init_target()?;
+    execute_test!(firmware_update_test, &opts, &transport);
+    execute_test!(get_boot_log_test, &opts, &transport);
+    Ok(())
+}

sw/host/tests/rescue/xmodem_protocol.rs
@@ -1,0 +1,110 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#![allow(clippy::bool_assert_comparison)]
+use anyhow::{Result, anyhow};
+use clap::Parser;
+use std::process::{Command, Stdio};
+use std::rc::Rc;
+use std::time::Duration;
+
+use opentitanlib::app::TransportWrapper;
+use opentitanlib::chip::boot_log::BootLog;
+use opentitanlib::execute_test;
+use opentitanlib::rescue::serial::RescueSerial;
+use opentitanlib::rescue::{EntryMode, Rescue, RescueMode};
+use opentitanlib::test_utils::init::InitializeTest;
+use opentitanlib::uart::console::UartConsole;
+
+#[derive(Debug, Parser)]
+struct Opts {
+    #[command(flatten)]
+    init: InitializeTest,
+
+    /// Console receive timeout.
+    #[arg(long, value_parser = humantime::parse_duration, default_value = "10s")]
+    timeout: Duration,
+
+    /// Firmware image
+    #[arg(long)]
+    firmware: String,
+}
+
+/// Checks that we can use the primitive xmodem tools from the `lrzsz` package
+/// to perform firmware rescue.
+fn firmware_update_test(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
+    let uart = transport.uart("console")?;
+    let rescue = RescueSerial::new(Rc::clone(&uart));
+
+    // It would be even cooler to do this in a python script or shell script, but
+    // opentitanlib is just too convenient for manipulating our test infrastructure.
+    // Nevertheless, we farm out the actual work to the `sx` (send xmodem) command.
+    rescue.enter(transport, EntryMode::Reset)?;
+
+    // Get the UART file descriptor and dup it to use as the child's stdin/stdout.
+    let fd = uart.borrow_fd()?;
+    let stdin = Stdio::from(rustix::io::dup(fd)?);
+    let stdout = Stdio::from(rustix::io::dup(fd)?);
+    let mut child = Command::new("sx")
+        .arg("--1k")
+        .arg(&opts.firmware)
+        .stdin(stdin)
+        .stdout(stdout)
+        .spawn()?;
+
+    let status = child.wait()?;
+    log::info!("Got xmodem exit code {status:?}");
+    rescue.reboot()?;
+
+    let capture = UartConsole::wait_for(&*uart, r"PASS!|BFV:([0-9A-Fa-f]{8})", opts.timeout)?;
+    if capture[0].starts_with("BFV") {
+        Err(anyhow!("Error: {}", capture[0]))
+    } else {
+        Ok(())
+    }
+}
+
+/// Checks that we can use the primitive xmodem tools from the `lrzsz` package
+/// to perform get the BootLog from the chip.
+fn get_boot_log_test(_opts: &Opts, transport: &TransportWrapper) -> Result<()> {
+    let uart = transport.uart("console")?;
+    let rescue = RescueSerial::new(Rc::clone(&uart));
+
+    // It would be even cooler to do this in a python script or shell script, but
+    // opentitanlib is just too convenient for manipulating our test infrastructure.
+    // Nevertheless, we farm out the actual work to the `rx` (recv xmodem) command.
+    rescue.enter(transport, EntryMode::Reset)?;
+    rescue.set_mode(RescueMode::BootLog)?;
+
+    let path = format!("boot_log-{}.bin", std::process::id());
+    let _ = std::fs::remove_file(&path);
+    log::info!("Receving boot_log into {path:?}");
+
+    // Get the UART file descriptor and dup it to use as the child's stdin/stdout.
+    let fd = uart.borrow_fd()?;
+    let stdin = Stdio::from(rustix::io::dup(fd)?);
+    let stdout = Stdio::from(rustix::io::dup(fd)?);
+    let mut child = Command::new("rx")
+        .arg("--with-crc")
+        .arg(&path)
+        .stdin(stdin)
+        .stdout(stdout)
+        .spawn()?;
+
+    let status = child.wait()?;
+    log::info!("Got xmodem exit code {status:?}");
+    let data = std::fs::read(&path)?;
+    let blog = BootLog::try_from(data.as_slice())?;
+    log::info!("BootLog = {blog:?}");
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let opts = Opts::parse();
+    opts.init.init_logging();
+    let transport = opts.init.init_target()?;
+    execute_test!(firmware_update_test, &opts, &transport);
+    execute_test!(get_boot_log_test, &opts, &transport);
+    Ok(())
+}