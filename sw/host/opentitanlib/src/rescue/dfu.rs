lowRISC/opentitan

[opentitanlib] Add a USB-DFU rescue client
1. Enhance the `UsbBackend` to allow for timeout-based detection of USB
   devices.
2. Give the `RescueParams` struct the ability to set SW_STRAP or
   GPIO-based triggers.
3. Define DFU constants and a `DfuOperations` trait to represent the
   basic DFU operations.
4. Create a UsbDfu rescue struct that implements the `Rescue` trait
   using `DfuOperations`.

Signed-off-by: Chris Frantz <cfrantz@google.com>

sw/host/opentitanlib/src/rescue/dfu.rs
@@ -1,0 +1,200 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::with_unknown;
+use anyhow::Result;
+use zerocopy::{AsBytes, FromBytes, FromZeroes};
+
+with_unknown! {
+    #[derive(FromBytes, FromZeroes, AsBytes)]
+    pub enum DfuState: u8 [default = Self::AppIdle] {
+      AppIdle = 0,
+      AppDetach= 1,
+      Idle = 2,
+      DnLoadSync = 3,
+      DnLoadBusy = 4,
+      DnLoadIdle = 5,
+      ManifestSync = 6,
+      Manifest = 7,
+      ManifestWaitReset = 8,
+      UpLoadIdle = 9,
+      Error = 10,
+    }
+}
+
+with_unknown! {
+    #[derive(FromBytes, FromZeroes, AsBytes)]
+    pub enum DfuError: u8 [default = Self::Ok] {
+      Ok = 0,
+      Target = 1,
+      File = 2,
+      Write = 3,
+      Erase = 4,
+      CheckErased = 5,
+      Prog = 6,
+      Verify = 7,
+      Address = 8,
+      NotDone = 9,
+      Firmware = 10,
+      Vendor = 11,
+      UsbReset = 12,
+      PowerOnReset = 13,
+      Unknown = 14,
+      StalledPkt = 15,
+    }
+}
+
+with_unknown! {
+    pub enum DfuRequest: u8 {
+      Detach = 0,
+      DnLoad = 1,
+      UpLoad = 2,
+      GetStatus = 3,
+      ClrStatus = 4,
+      GetState = 5,
+      Abort = 6,
+      BusReset = 7,
+    }
+}
+
+#[derive(Clone, Copy)]
+#[repr(u8)]
+pub enum DfuRequestType {
+    Out = 0x21, // direction=out, type=class, recipient=interface.
+    In = 0xA1,  // direction=in, type=class, recipient=interface.
+}
+
+impl From<DfuRequestType> for u8 {
+    fn from(val: DfuRequestType) -> Self {
+        val as u8
+    }
+}
+
+#[derive(Clone, FromBytes, FromZeroes, AsBytes, Default)]
+#[repr(C)]
+pub struct DfuStatus {
+    status: DfuError,
+    poll_timeout: [u8; 3],
+    state: DfuState,
+    string: u8,
+}
+
+impl std::error::Error for DfuError {}
+
+impl DfuStatus {
+    pub fn status(&self) -> std::result::Result<(), DfuError> {
+        match self.status {
+            DfuError::Ok => Ok(()),
+            e => Err(e),
+        }
+    }
+    pub fn poll_timeout(&self) -> u32 {
+        u32::from_le_bytes([
+            self.poll_timeout[0],
+            self.poll_timeout[1],
+            self.poll_timeout[2],
+            0,
+        ])
+    }
+    pub fn state(&self) -> DfuState {
+        self.state
+    }
+    pub fn string(&self) -> u8 {
+        self.string
+    }
+}
+
+pub trait DfuOperations {
+    fn write_control(
+        &self,
+        request_type: u8,
+        request: u8,
+        value: u16,
+        index: u16,
+        data: &[u8],
+    ) -> Result<usize>;
+
+    fn read_control(
+        &self,
+        request_type: u8,
+        request: u8,
+        value: u16,
+        index: u16,
+        data: &mut [u8],
+    ) -> Result<usize>;
+
+    fn get_interface(&self) -> u8;
+
+    /// Download (send) a block to the target device.
+    fn download(&self, data: &[u8]) -> Result<usize> {
+        self.write_control(
+            DfuRequestType::Out.into(),
+            DfuRequest::DnLoad.into(),
+            /*wValue=*/ 0,
+            /*wIndex=*/ self.get_interface() as u16,
+            data,
+        )
+    }
+
+    /// Upload (receive) a block from the target device.
+    fn upload(&self, data: &mut [u8]) -> Result<usize> {
+        self.read_control(
+            DfuRequestType::In.into(),
+            DfuRequest::UpLoad.into(),
+            /*wValue=*/ 0,
+            /*wIndex=*/ self.get_interface() as u16,
+            data,
+        )
+    }
+
+    /// Get the current DFU state.
+    fn get_state(&self) -> Result<DfuState> {
+        let mut buffer = [0u8];
+        self.read_control(
+            DfuRequestType::In.into(),
+            DfuRequest::GetState.into(),
+            /*wValue=*/ 0,
+            /*wIndex=*/ self.get_interface() as u16,
+            &mut buffer,
+        )?;
+        Ok(DfuState(buffer[0]))
+    }
+
+    /// Get the DFU status.
+    fn get_status(&self) -> Result<DfuStatus> {
+        let mut status = DfuStatus::default();
+        self.read_control(
+            DfuRequestType::In.into(),
+            DfuRequest::GetStatus.into(),
+            /*wValue=*/ 0,
+            /*wIndex=*/ self.get_interface() as u16,
+            status.as_bytes_mut(),
+        )?;
+        Ok(status)
+    }
+
+    /// Clear the DFU status.
+    fn clear_status(&self) -> Result<()> {
+        self.write_control(
+            DfuRequestType::Out.into(),
+            DfuRequest::ClrStatus.into(),
+            /*wValue=*/ 0,
+            /*wIndex=*/ self.get_interface() as u16,
+            &[],
+        )?;
+        Ok(())
+    }
+
+    /// Abort a DFU operation.
+    fn abort(&self) -> Result<()> {
+        self.write_control(
+            DfuRequestType::Out.into(),
+            DfuRequest::Abort.into(),
+            /*wValue=*/ 0,
+            /*wIndex=*/ self.get_interface() as u16,
+            &[],
+        )?;
+        Ok(())
+    }
+}

sw/host/opentitanlib/src/util/usb.rs
@@ -4,7 +4,7 @@
 
 use anyhow::{ensure, Context, Result};
 use rusb;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use crate::transport::TransportError;
 
@@ -20,8 +20,8 @@ impl UsbBackend {
     /// Scan the USB bus for a device matching VID/PID, and optionally also matching a serial
     /// number.
     pub fn scan(
-        usb_vid: u16,
-        usb_pid: u16,
+        usb_vid_pid: Option<(u16, u16)>,
+        usb_protocol: Option<(u8, u8, u8)>,
         usb_serial: Option<&str>,
     ) -> Result<Vec<(rusb::Device<rusb::GlobalContext>, String)>> {
         let mut devices = Vec::new();
@@ -39,11 +39,42 @@ impl UsbBackend {
                     continue;
                 }
             };
-            if descriptor.vendor_id() != usb_vid {
-                continue;
+
+            if let Some((vid, pid)) = usb_vid_pid {
+                if descriptor.vendor_id() != vid {
+                    continue;
+                }
+                if descriptor.product_id() != pid {
+                    continue;
+                }
             }
-            if descriptor.product_id() != usb_pid {
-                continue;
+            if let Some((class, subclass, protocol)) = usb_protocol {
+                let config = match device.active_config_descriptor() {
+                    Ok(desc) => desc,
+                    Err(e) => {
+                        deferred_log_messages.push(format!(
+                            "Could not read config descriptor for device at bus={} address={}: {}",
+                            device.bus_number(),
+                            device.address(),
+                            e,
+                        ));
+                        continue;
+                    }
+                };
+                let mut found = false;
+                for intf in config.interfaces() {
+                    for desc in intf.descriptors() {
+                        if desc.class_code() == class
+                            && desc.sub_class_code() == subclass
+                            && desc.protocol_code() == protocol
+                        {
+                            found = true;
+                        }
+                    }
+                }
+                if !found {
+                    continue;
+                }
             }
             let handle = match device.open() {
                 Ok(handle) => handle,
@@ -57,6 +88,7 @@ impl UsbBackend {
                     continue;
                 }
             };
+
             let serial_number = match handle.read_serial_number_string_ascii(&descriptor) {
                 Ok(sn) => sn,
                 Err(e) => {
@@ -88,13 +120,12 @@ impl UsbBackend {
         for s in deferred_log_messages {
             log::log!(severity, "{}", s);
         }
-
         Ok(devices)
     }
 
     /// Create a new UsbBackend.
     pub fn new(usb_vid: u16, usb_pid: u16, usb_serial: Option<&str>) -> Result<Self> {
-        let mut devices = UsbBackend::scan(usb_vid, usb_pid, usb_serial)?;
+        let mut devices = UsbBackend::scan(Some((usb_vid, usb_pid)), None, usb_serial)?;
         ensure!(!devices.is_empty(), TransportError::NoDevice);
         ensure!(devices.len() == 1, TransportError::MultipleDevices);
 
@@ -105,6 +136,50 @@ impl UsbBackend {
             serial_number,
             timeout: Duration::from_millis(500),
         })
+    }
+
+    pub fn from_interface(
+        class: u8,
+        subclass: u8,
+        protocol: u8,
+        usb_serial: Option<&str>,
+    ) -> Result<Self> {
+        Self::from_interface_with_timeout(class, subclass, protocol, usb_serial, Duration::ZERO)
+    }
+
+    pub fn from_interface_with_timeout(
+        class: u8,
+        subclass: u8,
+        protocol: u8,
+        usb_serial: Option<&str>,
+        timeout: Duration,
+    ) -> Result<Self> {
+        let deadline = Instant::now() + timeout;
+        loop {
+            let mut devices =
+                UsbBackend::scan(None, Some((class, subclass, protocol)), usb_serial)?;
+            if devices.is_empty() {
+                if Instant::now() < deadline {
+                    std::thread::sleep(Duration::from_millis(100));
+                    continue;
+                } else {
+                    return Err(TransportError::NoDevice.into());
+                }
+            }
+            ensure!(devices.len() == 1, TransportError::MultipleDevices);
+
+            let (device, serial_number) = devices.remove(0);
+            return Ok(UsbBackend {
+                handle: device.open().context("USB open error")?,
+                device,
+                serial_number,
+                timeout: Duration::from_millis(500),
+            });
+        }
+    }
+
+    pub fn handle(&self) -> &rusb::DeviceHandle<rusb::GlobalContext> {
+        &self.handle
     }
 
     pub fn get_vendor_id(&self) -> u16 {
@@ -174,6 +249,10 @@ impl UsbBackend {
         self.handle
             .read_string_descriptor_ascii(idx)
             .context("USB error")
+    }
+
+    pub fn reset(&self) -> Result<()> {
+        self.handle.reset().context("USB Error")
     }
 
     //

pavona/pavona

(cherry picked from commit b17c4f0495531073000f660df939c497e2d4f708)

sw/host/opentitanlib/src/rescue/dfu.rs
@@ -1,0 +1,200 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::with_unknown;
+use anyhow::Result;
+use zerocopy::{FromBytes, Immutable, IntoBytes};
+
+with_unknown! {
+    #[derive(FromBytes, Immutable, IntoBytes)]
+    pub enum DfuState: u8 [default = Self::AppIdle] {
+      AppIdle = 0,
+      AppDetach= 1,
+      Idle = 2,
+      DnLoadSync = 3,
+      DnLoadBusy = 4,
+      DnLoadIdle = 5,
+      ManifestSync = 6,
+      Manifest = 7,
+      ManifestWaitReset = 8,
+      UpLoadIdle = 9,
+      Error = 10,
+    }
+}
+
+with_unknown! {
+    #[derive(FromBytes, Immutable, IntoBytes)]
+    pub enum DfuError: u8 [default = Self::Ok] {
+      Ok = 0,
+      Target = 1,
+      File = 2,
+      Write = 3,
+      Erase = 4,
+      CheckErased = 5,
+      Prog = 6,
+      Verify = 7,
+      Address = 8,
+      NotDone = 9,
+      Firmware = 10,
+      Vendor = 11,
+      UsbReset = 12,
+      PowerOnReset = 13,
+      Unknown = 14,
+      StalledPkt = 15,
+    }
+}
+
+with_unknown! {
+    pub enum DfuRequest: u8 {
+      Detach = 0,
+      DnLoad = 1,
+      UpLoad = 2,
+      GetStatus = 3,
+      ClrStatus = 4,
+      GetState = 5,
+      Abort = 6,
+      BusReset = 7,
+    }
+}
+
+#[derive(Clone, Copy)]
+#[repr(u8)]
+pub enum DfuRequestType {
+    Out = 0x21, // direction=out, type=class, recipient=interface.
+    In = 0xA1,  // direction=in, type=class, recipient=interface.
+}
+
+impl From<DfuRequestType> for u8 {
+    fn from(val: DfuRequestType) -> Self {
+        val as u8
+    }
+}
+
+#[derive(Clone, FromBytes, Immutable, IntoBytes, Default)]
+#[repr(C)]
+pub struct DfuStatus {
+    status: DfuError,
+    poll_timeout: [u8; 3],
+    state: DfuState,
+    string: u8,
+}
+
+impl std::error::Error for DfuError {}
+
+impl DfuStatus {
+    pub fn status(&self) -> std::result::Result<(), DfuError> {
+        match self.status {
+            DfuError::Ok => Ok(()),
+            e => Err(e),
+        }
+    }
+    pub fn poll_timeout(&self) -> u32 {
+        u32::from_le_bytes([
+            self.poll_timeout[0],
+            self.poll_timeout[1],
+            self.poll_timeout[2],
+            0,
+        ])
+    }
+    pub fn state(&self) -> DfuState {
+        self.state
+    }
+    pub fn string(&self) -> u8 {
+        self.string
+    }
+}
+
+pub trait DfuOperations {
+    fn write_control(
+        &self,
+        request_type: u8,
+        request: u8,
+        value: u16,
+        index: u16,
+        data: &[u8],
+    ) -> Result<usize>;
+
+    fn read_control(
+        &self,
+        request_type: u8,
+        request: u8,
+        value: u16,
+        index: u16,
+        data: &mut [u8],
+    ) -> Result<usize>;
+
+    fn get_interface(&self) -> u8;
+
+    /// Download (send) a block to the target device.
+    fn download(&self, data: &[u8]) -> Result<usize> {
+        self.write_control(
+            DfuRequestType::Out.into(),
+            DfuRequest::DnLoad.into(),
+            /*wValue=*/ 0,
+            /*wIndex=*/ self.get_interface() as u16,
+            data,
+        )
+    }
+
+    /// Upload (receive) a block from the target device.
+    fn upload(&self, data: &mut [u8]) -> Result<usize> {
+        self.read_control(
+            DfuRequestType::In.into(),
+            DfuRequest::UpLoad.into(),
+            /*wValue=*/ 0,
+            /*wIndex=*/ self.get_interface() as u16,
+            data,
+        )
+    }
+
+    /// Get the current DFU state.
+    fn get_state(&self) -> Result<DfuState> {
+        let mut buffer = [0u8];
+        self.read_control(
+            DfuRequestType::In.into(),
+            DfuRequest::GetState.into(),
+            /*wValue=*/ 0,
+            /*wIndex=*/ self.get_interface() as u16,
+            &mut buffer,
+        )?;
+        Ok(DfuState(buffer[0]))
+    }
+
+    /// Get the DFU status.
+    fn get_status(&self) -> Result<DfuStatus> {
+        let mut status = DfuStatus::default();
+        self.read_control(
+            DfuRequestType::In.into(),
+            DfuRequest::GetStatus.into(),
+            /*wValue=*/ 0,
+            /*wIndex=*/ self.get_interface() as u16,
+            status.as_mut_bytes(),
+        )?;
+        Ok(status)
+    }
+
+    /// Clear the DFU status.
+    fn clear_status(&self) -> Result<()> {
+        self.write_control(
+            DfuRequestType::Out.into(),
+            DfuRequest::ClrStatus.into(),
+            /*wValue=*/ 0,
+            /*wIndex=*/ self.get_interface() as u16,
+            &[],
+        )?;
+        Ok(())
+    }
+
+    /// Abort a DFU operation.
+    fn abort(&self) -> Result<()> {
+        self.write_control(
+            DfuRequestType::Out.into(),
+            DfuRequest::Abort.into(),
+            /*wValue=*/ 0,
+            /*wIndex=*/ self.get_interface() as u16,
+            &[],
+        )?;
+        Ok(())
+    }
+}

sw/host/opentitanlib/src/util/usb.rs
@@ -4,7 +4,7 @@
 
 use anyhow::{Context, Result, ensure};
 use rusb::{self, UsbContext};
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use crate::transport::TransportError;
 
@@ -20,8 +20,8 @@ impl UsbBackend {
     /// Scan the USB bus for a device matching VID/PID, and optionally also matching a serial
     /// number.
     pub fn scan(
-        usb_vid: u16,
-        usb_pid: u16,
+        usb_vid_pid: Option<(u16, u16)>,
+        usb_protocol: Option<(u8, u8, u8)>,
         usb_serial: Option<&str>,
     ) -> Result<Vec<(rusb::Device<rusb::Context>, String)>> {
         let mut devices = Vec::new();
@@ -39,11 +39,42 @@ impl UsbBackend {
                     continue;
                 }
             };
-            if descriptor.vendor_id() != usb_vid {
-                continue;
+
+            if let Some((vid, pid)) = usb_vid_pid {
+                if descriptor.vendor_id() != vid {
+                    continue;
+                }
+                if descriptor.product_id() != pid {
+                    continue;
+                }
             }
-            if descriptor.product_id() != usb_pid {
-                continue;
+            if let Some((class, subclass, protocol)) = usb_protocol {
+                let config = match device.active_config_descriptor() {
+                    Ok(desc) => desc,
+                    Err(e) => {
+                        deferred_log_messages.push(format!(
+                            "Could not read config descriptor for device at bus={} address={}: {}",
+                            device.bus_number(),
+                            device.address(),
+                            e,
+                        ));
+                        continue;
+                    }
+                };
+                let mut found = false;
+                for intf in config.interfaces() {
+                    for desc in intf.descriptors() {
+                        if desc.class_code() == class
+                            && desc.sub_class_code() == subclass
+                            && desc.protocol_code() == protocol
+                        {
+                            found = true;
+                        }
+                    }
+                }
+                if !found {
+                    continue;
+                }
             }
             let handle = match device.open() {
                 Ok(handle) => handle,
@@ -57,6 +88,7 @@ impl UsbBackend {
                     continue;
                 }
             };
+
             let serial_number = match handle.read_serial_number_string_ascii(&descriptor) {
                 Ok(sn) => sn,
                 Err(e) => {
@@ -88,13 +120,12 @@ impl UsbBackend {
         for s in deferred_log_messages {
             log::log!(severity, "{}", s);
         }
-
         Ok(devices)
     }
 
     /// Create a new UsbBackend.
     pub fn new(usb_vid: u16, usb_pid: u16, usb_serial: Option<&str>) -> Result<Self> {
-        let mut devices = UsbBackend::scan(usb_vid, usb_pid, usb_serial)?;
+        let mut devices = UsbBackend::scan(Some((usb_vid, usb_pid)), None, usb_serial)?;
         ensure!(!devices.is_empty(), TransportError::NoDevice);
         ensure!(devices.len() == 1, TransportError::MultipleDevices);
 
@@ -105,6 +136,50 @@ impl UsbBackend {
             serial_number,
             timeout: Duration::from_millis(500),
         })
+    }
+
+    pub fn from_interface(
+        class: u8,
+        subclass: u8,
+        protocol: u8,
+        usb_serial: Option<&str>,
+    ) -> Result<Self> {
+        Self::from_interface_with_timeout(class, subclass, protocol, usb_serial, Duration::ZERO)
+    }
+
+    pub fn from_interface_with_timeout(
+        class: u8,
+        subclass: u8,
+        protocol: u8,
+        usb_serial: Option<&str>,
+        timeout: Duration,
+    ) -> Result<Self> {
+        let deadline = Instant::now() + timeout;
+        loop {
+            let mut devices =
+                UsbBackend::scan(None, Some((class, subclass, protocol)), usb_serial)?;
+            if devices.is_empty() {
+                if Instant::now() < deadline {
+                    std::thread::sleep(Duration::from_millis(100));
+                    continue;
+                } else {
+                    return Err(TransportError::NoDevice.into());
+                }
+            }
+            ensure!(devices.len() == 1, TransportError::MultipleDevices);
+
+            let (device, serial_number) = devices.remove(0);
+            return Ok(UsbBackend {
+                handle: device.open().context("USB open error")?,
+                device,
+                serial_number,
+                timeout: Duration::from_millis(500),
+            });
+        }
+    }
+
+    pub fn handle(&self) -> &rusb::DeviceHandle<rusb::Context> {
+        &self.handle
     }
 
     pub fn get_vendor_id(&self) -> u16 {
@@ -174,6 +249,10 @@ impl UsbBackend {
         self.handle
             .read_string_descriptor_ascii(idx)
             .context("USB error")
+    }
+
+    pub fn reset(&self) -> Result<()> {
+        self.handle.reset().context("USB Error")
     }
 
     //