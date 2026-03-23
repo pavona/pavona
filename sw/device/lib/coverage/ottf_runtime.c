lowRISC/opentitan

[sw,cov] Add on-device OTTF coverage runtime
This change adds a new on-device coverage runtime with OTTF transport.

When coverage is enabled, the runtime can collect profile counter data from the device and send it over the console.
On the host, these raw counter values can be combined with static profile data to recreate the full LLVM profile.

The coverage runtime consists of the following components:
* `api.h`: Provides a set of shared C APIs implemented by transport runtimes.
* `api_asm.h`: Provides APIs for assembly code.
* `printer.h`/`printer.c`: Implements the shared logic for different runtimes.
* Linker scripts (`bss.ld`, `info.ld`, `rodata.ld`): Define special sections for LLVM profile data, counters, and build ID.

OTTF transport implementation is also included:
* `ottf_runtime.c`: Integrates the coverage printer with the OTTF console for data output.
* `ottf_runtime_skip.c`: A no-op version of the runtime for builds where coverage is enabled but not instrumented.

Change-Id: I2e4b135a58b93a257fc37880700e23f4b99defc4
Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>

sw/device/lib/coverage/ottf_runtime.c
@@ -1,0 +1,32 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "sw/device/lib/coverage/printer.h"
+#include "sw/device/lib/runtime/log.h"
+#include "sw/device/lib/runtime/print.h"
+#include "sw/device/lib/testing/test_framework/ottf_console.h"
+
+void coverage_transport_init(void) {
+  ottf_console_init();
+  base_printf("COVERAGE:OTTF\r\n");
+}
+
+void coverage_report(void) {
+  if (coverage_is_valid()) {
+    base_printf("== COVERAGE PROFILE START ==\r\n");
+    coverage_printer_run();
+    base_printf("== COVERAGE PROFILE END ==\r\n");
+  } else {
+    base_printf("== COVERAGE PROFILE DUMPED ==\r\n");
+  }
+}
+
+void coverage_printer_sink(const void *data, size_t size) {
+  for (size_t i = 0; i < size; ++i) {
+    base_printf("%02x", ((uint8_t *)data)[i]);
+  }
+}

pavona/pavona

(cherry picked from commit 7d450f82cc4519e2de169431b07ce94a1e62351e)

sw/device/lib/coverage/ottf_runtime.c
@@ -1,0 +1,32 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "sw/device/lib/coverage/printer.h"
+#include "sw/device/lib/runtime/log.h"
+#include "sw/device/lib/runtime/print.h"
+#include "sw/device/lib/testing/test_framework/ottf_console.h"
+
+void coverage_transport_init(void) {
+  ottf_console_init();
+  base_printf("COVERAGE:OTTF\r\n");
+}
+
+void coverage_report(void) {
+  if (coverage_is_valid()) {
+    base_printf("== COVERAGE PROFILE START ==\r\n");
+    coverage_printer_run();
+    base_printf("== COVERAGE PROFILE END ==\r\n");
+  } else {
+    base_printf("== COVERAGE PROFILE INVALID ==\r\n");
+  }
+}
+
+void coverage_printer_sink(const void *data, size_t size) {
+  for (size_t i = 0; i < size; ++i) {
+    base_printf("%02x", ((uint8_t *)data)[i]);
+  }
+}