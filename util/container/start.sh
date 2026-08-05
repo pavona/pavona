75cda5b942b46bcd0c52f3ffbcb445e51ec82e2f
Nicholas Gildenhuys <ngildenhuys@zerorisc.com>
2026-08-05 14:18:46 -0700

    [util] changing all doc references and python invocations to use the enviroments bash
    Signed-off-by: Nicholas Gildenhuys <ngildenhuys@zerorisc.com>

--- util/container/start.sh
+++ util/container/start.sh
@@ -14,4 +14,4 @@ usermod -o -u "$DEV_UID" dev >/dev/null 2>&1
 test -f "${USER_CONFIG}" && export BASH_ENV=${USER_CONFIG}
 
 cd /home/dev || exit
-exec gosu dev:dev /bin/bash -c "$@"
+exec gosu dev:dev bash -c "$@"
