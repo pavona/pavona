9832c0bb0eb68d1d5f8ff252091e873ca9e5f084
Ryan Torok <rtorok@zerorisc.com>
2026-09-15 11:41:59 -0400

    [rv_dm,test] Fix rv_dm_lc_disabled_tl test
    This test was broken by `6230bb65`, which removed the explicity Debug ROM entry
    in the Ibex reset ePMP configuration from Egret. This happened because the test
    attempts to check the debug ROM is operational in the correct lifecycle states
    by writing to it from Ibex, which, unlike RV_DM itself, is still subject to
    ePMP constraints. This commit fixes this defect by adding an explicit ePMP
    entry for the debug memory in the test script, and also upgrades the test to
    use DT for multi-top compatibility.
    
    Signed-off-by: Ryan Torok <rtorok@zerorisc.com>

--- sw/device/tests/rv_dm_lc_disabled.c
+++ sw/device/tests/rv_dm_lc_disabled.c
@@ -2,16 +2,14 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#include "hw/top/dt/rv_dm.h"
 #include "sw/device/lib/runtime/log.h"
 #include "sw/device/lib/testing/lc_ctrl_testutils.h"
 #include "sw/device/lib/testing/test_framework/check.h"
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
+#include "sw/device/silicon_creator/lib/drivers/epmp.h"
 
-#include "hw/top_egret/sw/autogen/top_egret.h"
-
-// This is an ad-hoc definition rather than DIF because we normally do not
-// communicate with RV-DM from Ibex core, but use JTAG instead.
-#define RV_DM_DATAADDR_0_REG_OFFSET 0x380
+#include "hw/top/rv_dm_regs.h"
 
 enum {
   kTestData = 0xdeadbeef,
@@ -26,7 +24,8 @@ void ottf_load_store_fault_handler(uint32_t *exc_info) {
 }
 
 status_t execute_test(bool debug_func) {
-  mmio_region_t region = mmio_region_from_addr(TOP_EGRET_RV_DM_MEM_BASE_ADDR);
+  mmio_region_t region =
+      mmio_region_from_addr(dt_rv_dm_reg_block(kDtRvDm, kDtRvDmRegBlockMem));
 
   // Attempt to write to RV-DM register and read.
   access_exception_seen = false;
@@ -41,9 +40,19 @@ status_t execute_test(bool debug_func) {
 }
 
 bool test_main(void) {
+  // Enable access to the RV_DM memory in ePMP. Slot 6 is used because slots
+  // 0-5 are used to manage access to the boot stages.
+  epmp_region_t rv_dm_region = {
+      .start = dt_rv_dm_reg_block(kDtRvDm, kDtRvDmRegBlockMem),
+      .end = dt_rv_dm_reg_block(kDtRvDm, kDtRvDmRegBlockMem) +
+             RV_DM_ROM_REG_OFFSET + RV_DM_ROM_SIZE_BYTES,
+  };
+  epmp_set_napot(6, rv_dm_region, kEpmpPermLockedReadWrite);
+
   dif_lc_ctrl_t lc;
-  CHECK_DIF_OK(dif_lc_ctrl_init(
-      mmio_region_from_addr(TOP_EGRET_LC_CTRL_REGS_BASE_ADDR), &lc));
+  CHECK_DIF_OK(dif_lc_ctrl_init(mmio_region_from_addr(dt_lc_ctrl_reg_block(
+                                    kDtLcCtrl, kDtLcCtrlRegBlockRegs)),
+                                &lc));
 
   bool debug_func = false;
   CHECK_STATUS_OK(lc_ctrl_testutils_debug_func_enabled(&lc, &debug_func));
